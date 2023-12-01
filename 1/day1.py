5a186f44de98d034c4fd1924ac9c65dfe50696bc
Sasha "Sato" Fourmage <sato2806@proton.me>
2023-12-01 14:04:40 +0100

    Day 1 Complete

--- 1/day1.py
+++ 1/day1.py
@@ -7,16 +7,30 @@ Created on Fri Dec  1 12:44:30 2023
 AdventOfCode2023: day 1
 """
 
+numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
+#'zero' added so index == digit value
+
+def extract_str_digits(line):
+    digits=[]
+    for i in numbers:
+        if i in line:
+            digits+=[[line.find(i), numbers.index(i)]]+[[line.rfind(i), numbers.index(i)]]
+    return digits
+
 def calibrate(line):
-    value=[]
+    value=extract_str_digits(line)
     for i in line:
         try: 
-            value+= [int(i)]
+            value+=[[line.index(i), int(i)]]+[[line.rindex(i), int(i)]]
         except:
             pass
+    value.sort()
+    
 
-    calibration_value = str(value[0]) + str(value[-1])
+    calibration_value = str(value[0][1]) + str(value[-1][1])
     return calibration_value
+
+    
 
 def main(): 
     f = open('1.txt', 'r')  
