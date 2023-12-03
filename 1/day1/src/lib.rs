e232213c6a45ff8c03fa00a3bd91bf439f854687
SatoSq <132358561+SatoSq@users.noreply.github.com>
2023-12-03 13:20:02 +0100

    Refactor day1_rust

--- 1/day1/src/lib.rs
+++ 1/day1/src/lib.rs
@@ -1,18 +1,19 @@
-pub fn part1(text: &str) -> u32 {
-    let mut sum = 0;
-
-    // text.lines().map(|line| {
-    //     //for each line in the text
-    //     let line: Vec<char> = line.chars().filter(|i: &char| i.is_digit(10)).collect();
-    //     let value = (String::from(line.first().unwrap())).push(String::from(line.last().unwrap()));
-    //     value
-    // }).sum()
-
-    for line in text.lines() {
-        let line: Vec<char> = line.chars().filter(|i: &char| i.is_digit(10)).collect();
-        let value = format!("{}{}", line.first().unwrap(), line.last().unwrap()).parse::<u32>().unwrap();
-        sum+=value
-    }
-
-    sum
+fn part1(text: &str) -> u32 {
+     text.lines().map(|line| {
+         //for each line in the text
+         let line: Vec<char> = line
+            .chars()
+            .filter(char::is_ascii_digit)
+            .collect();
+         format!(
+            "{}{}",
+            line.first()
+                .unwrap_or(&'0'), 
+            line.last()
+                .unwrap_or(&'0')
+        )
+        .parse::<u32>()
+        .expect("Concatenate")
+     })
+     .sum()
 }
