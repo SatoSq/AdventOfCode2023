995afe27972e3d85a41fff621cbceab9a449daea
SatoSq <132358561+SatoSq@users.noreply.github.com>
2023-12-03 03:56:20 +0100

    Delete target directory

--- 1/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/target/debug/day1: /home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/1.txt /home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/src/lib.rs /home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/src/main.rs
--- 1/day1/target/debug/libday1.d
+++
@@ -1,1 +0,0 @@
-/home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/target/debug/libday1.rlib: /home/sato/Documents/AdventOfCode2023/AdventOfCode2023/1/day1/src/lib.rs
